611bc018ba291650b723e1a477bcf4b1ea38698b
Cartha007 <g98kattah@gmail.com>
2024-07-23 17:36:51 +0000

    Added main func

--- imdbscraper.py
+++ imdbscraper.py
@@ -12,3 +12,26 @@ def check_internet_connection():
     except requests.ConnectionError:
         print("No internet connection available. You need an internet connection to run this script.")
         sys.exit()
+
+
+def main():
+    search_url = "https://www.imdb.com/find/?q="
+    search_ref = "&ref_=nv_sr_sm"
+    print('Enter movie name to search')
+    search = input('>').split(" ")
+    search_url += '%20'.join(search)
+    print(search_url)
+
+
+
+if __name__ == '__main__':
+    # print('Starting script...')
+    check_internet_connection()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(e)
+    finally:
+        print('Script ended.')
